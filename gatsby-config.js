fef4b4f850d7cedd0e9735f7544578d84e4ac87d
Preston Lai <preston1lai@gmail.com>
2022-08-24 17:46:59 -0700

    changed website title

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
         // You can overwrite values here that are used for the SEO component
         // You can also add new values here to query them like usual
         // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-        siteTitle: `Cara`,
-        siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-        siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-        siteUrl: `https://cara.lekoarts.de`,
+        siteTitle: `Preston`,
+        siteTitleAlt: `Preston Lai's Website`,
+        siteHeadline: `Preston Lai's Website`,
+        siteUrl: `prestonlai.me`,
         siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
         siteLanguage: `en`,
         siteImage: `/banner.jpg`,
-        author: `@lekoarts_de`,
+        author: `Preston`,
     },
     plugins: [{
             resolve: `@lekoarts/gatsby-theme-cara`,
